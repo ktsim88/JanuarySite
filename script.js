ef6f7029ad0bd5fec6d9c0c74b0ed280e362f899
ktsim88 <526ktsim@frhsd.com>
2025-01-09 09:52:11 -0500

    added js greeting

---
+++ script.js
@@ -0,0 +1,4 @@
+// user greeting
+let userName = prompt('Welcome! Who are you?')
+userName ? document.getElementById('greeting').innerText = (`Hello ${userName}!`) : document.getElementById('greeting').innerText = 'Hello!'
+
