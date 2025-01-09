6b99eca3fb5de9c8158bff3f24410f46d3444d53
ktsim88 <526ktsim@frhsd.com>
2025-01-09 10:30:39 -0500

    finished layout for small screens and added user's score

--- script.js
+++ script.js
@@ -2,3 +2,6 @@
 let userName = prompt('Welcome! Who are you?')
 userName ? document.getElementById('greeting').innerText = (`Hello ${userName}!`) : document.getElementById('greeting').innerText = 'Hello!'
 
+//user score
+let userScore = userName
+userScore ? document.getElementById('score').innerText = (`${userName}'s Score`) : document.getElementById('score').innerText = 'Your Score'
