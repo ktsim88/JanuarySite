df40a2facaf1e3d768723ccc3edac50c4708de8e
ktsim88 <526ktsim@frhsd.com>
2025-01-31 03:11:20 +0000

    Win counter and result counter added

--- script.js
+++ script.js
@@ -12,29 +12,38 @@ const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstra
 //Vars for USerScore
 let userScoreDisplay = document.getElementById('userScoreDisplay')
 let userScore = 0
+let userWinsCount = 0
 let userSelect = document.getElementById('userSelect')
 let userImage = document.getElementById('userImage')
+let numUserWinsDisplay = document.getElementById('numUserWins')
+
 
 //vars for computerscore
 let compScore = 0
+let compWinsCount = 0
 let compScoreDisplay = document.getElementById('compScoreDisplay')
 let compSelect = document.getElementById('computerSelect')
 let compImage = document.getElementById('compImage')
+let numCompWinsDisplay = document.getElementById('numCompWins')
 
 
 let winner = document.getElementById('winner')
 // rounds
-let round = 1;
+let round = 0;
 let roundDisplay = document.getElementById('round'); 
 roundDisplay.innerText = `Round: ${round}/5`;
 // game over word
 let finalWinner = document.getElementById('finalWinner')
+
 // winner functions
 function userWins() {
   userScore++
   userScoreDisplay.innerText = userScore
   winner.innerText = 'User Wins'
   roundsPlayed()
+  
+  userWinsCount++;
+  numUserWinsDisplay.innerText = `Wins: ${userWinsCount}`
 }
 
 function compWins() {
@@ -42,6 +51,9 @@ function compWins() {
   compScoreDisplay.innerText = compScore
   winner.textContent = 'Computer wins this round!'
   roundsPlayed()
+
+  compWinsCount++;
+  numCompWinsDisplay.innerText = `Wins: ${compWinsCount}`
 }
 
 function tie() {
@@ -52,19 +64,10 @@ function tie() {
   compScoreDisplay.innerText = compScore
   winner.innerText = 'its a tie!'
   roundsPlayed()
-}
-
-function image() {
-  if (userSelect === 'scissors' || compSelect === 'scissors') {
-    userImage.src = 'imgs/scissors.png'
-    compImage.src = 'imgs/scissors.png'
-  } else if (userSelect === 'paper' || compSelect === 'paper') {
-    userImage.src = 'imgs/paper.png'
-    compImage.src = 'imgs/paper.png'
-  } else if (userSelect === 'rock' || compSelect === 'rock') {
-    userImage.src = 'imgs/rock.png'
-    compImage.src = 'imgs/rock.png'
-  }
+  userWinsCount++;
+  numUserWinsDisplay.innerText = `Wins: ${userWinsCount}`
+  compWinsCount++;
+  numCompWinsDisplay.innerText = `Wins: ${compWinsCount}`
 }
   function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
@@ -104,7 +107,7 @@ function playGame(userChoice) {
     userSelect.innerText = `You threw: ${userChoice}`;
     compSelect.innerText = `The computer threw: ${computerChoice}`;
   determineWinner(userChoice, computerChoice);
-  image();
+  gameOver();
 }
 
 // rounds
@@ -112,18 +115,35 @@ function roundsPlayed() {
   round++
   roundDisplay.innerText = `Round: ${round}/5`
 }
+// game over 
+function gameOver() {
+  if (userWinsCount >= 3) {
+    finalWinner.innerText = 'Congrats! You won the game! Click Reset to Play Again'
+  } else if (compWinsCount >= 3) {
+    finalWinner.innerText = 'Dang! How did you lose against the computer!?! Click Reset to Play Again'
+  } else if (userWinsCount === 3 && compWinsCount === 3) {
+    finalWinner.innerText = 'Play one more round.'
+  }
+
+}
+
 
 //reset game
 function resetGame() {
-  userScore = 0;
-  compScore = 0;
-  round = 0;
-  userScoreDisplay.innerText = userScore;
-  compScoreDisplay.innerText = compScore;
-  roundDisplay.innerText = `Round: ${round}/5`;
-  winner.innerText = '';
-  userSelect.innerText = '';
-  compSelect.innerText = '';
+  userScore = 0
+  compScore = 0
+  round = 0
+  userScoreDisplay.innerText = userScore
+  compScoreDisplay.innerText = compScore
+  roundDisplay.innerText = `Round: ${round}/5`
+  winner.innerText = ''
+  userSelect.innerText = ''
+  compSelect.innerText = ''
   userImage.src = "imgs/2.png";  
   compImage.src = "imgs/1.png";
+  compWinsCount = 0
+  userWinsCount = 0
+  finalWinner.innerText = ''
+  numUserWinsDisplay.innerText = ''
+  numCompWinsDisplay.innerText = ''
 }
